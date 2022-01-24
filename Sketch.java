19b1ad306eef87fad9000e8392e0516e8ce0cfdc
Ahastan <ahastan.surees23@ycdsbk12.ca>
2022-01-23 23:25:02 -0500

    level 3

--- Sketch.java
+++ Sketch.java
@@ -27,7 +27,19 @@ public class Sketch extends PApplet {
     {
       circleX = width * i / circleY.length;
       ellipse(circleX, circleY[i], 5, 5);
-      circleY[i]+=2;
+      circleY[i] += 2;
+      
+      if (keyPressed)
+      {
+        if(keyCode == UP)
+        {
+          circleY[i] += 4;
+        }
+        else if (keyCode == DOWN)
+        {
+          circleY[i] += 0.5;
+        }
+    }
 
       if (circleY[i] > height)
       {
@@ -35,6 +47,7 @@ public class Sketch extends PApplet {
         bottom[(int) circleX]++;
       }
     }
+    
     for (int k = 0; k < bottom.length; k++)
     {
       for (int p = 0; p < bottom[k]; p++)
